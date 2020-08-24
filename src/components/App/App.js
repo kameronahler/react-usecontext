9ca7095bc93d8678beed69184e37f152d9d55aae
Kameron Ahler <kamahler@gmail.com>
2020-08-23 23:04:59 -0600

    setup context provider and basic markup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.scss'
+
+export const ThemeContext = React.createContext()
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = useState(() => {
+    return true
+  })
+
+  const [buttonAriaLabel, setButtonAriaLabel] = useState(() => {
+    return 'Change theme to light'
+  })
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <header>
+          <h2>Toggle theme</h2>
+        </header>
+        <button aria-label={buttonAriaLabel} className='button'>
+          <span
+            aria-hidden='true'
+            className='button__label button__label--dark'
+          >
+            {darkTheme ? 'Dark mode' : 'Light mode'}
+          </span>
+          <div className='button__toggle-wrapper'>
+            <div
+              className={`button__indicator
+                ${
+                  darkTheme
+                    ? 'button__indicator--dark'
+                    : 'button__indicator--light'
+                }`}
+            ></div>
+          </div>
+        </button>
+        <h1>Hello world</h1>
+      </ThemeContext.Provider>
+    </>
   )
 }
