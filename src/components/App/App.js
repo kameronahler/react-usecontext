62d6dadae7cb816fcf4024b744a1d6f736cc017f
Kameron Ahler <kamahler@gmail.com>
2020-08-23 23:34:53 -0600

    basic styles for excercise

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import 'normalize.css'
 import './App.scss'
 
 export const ThemeContext = React.createContext()
@@ -15,28 +16,29 @@ export default function App() {
   return (
     <>
       <ThemeContext.Provider value={darkTheme}>
-        <header>
-          <h2>Toggle theme</h2>
-        </header>
-        <button aria-label={buttonAriaLabel} className='button'>
-          <span
-            aria-hidden='true'
-            className='button__label button__label--dark'
-          >
-            {darkTheme ? 'Dark mode' : 'Light mode'}
-          </span>
-          <div className='button__toggle-wrapper'>
-            <div
-              className={`button__indicator
-                ${
-                  darkTheme
-                    ? 'button__indicator--dark'
-                    : 'button__indicator--light'
-                }`}
-            ></div>
+        <section className='grid'>
+          <div className='grid__left'>
+            <header>
+              <h1>Toggle theme</h1>
+            </header>
+            <button aria-label={buttonAriaLabel} className='toggle'>
+              <div className='toggle__switch-wrapper'>
+                <div className='toggle__switch'></div>
+              </div>
+            </button>
           </div>
-        </button>
-        <h1>Hello world</h1>
+          <div className='grid__right'>
+            <div className='example'>
+              <h2 className='example__heading'>Example</h2>
+              <p className='example__body'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi odio aliquid sequi fugiat beatae expedita, dolorem
+                sint libero quam amet voluptatum voluptatem corrupti in illo.
+                Debitis fugiat hic unde iste.
+              </p>
+            </div>
+          </div>
+        </section>
       </ThemeContext.Provider>
     </>
   )
